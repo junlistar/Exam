a34c82dff1fd082f4315a465acb81d734b454686
EDAN <EDAN@DESKTOP-99AUGL9>
2017-12-25 16:37:48 +0800

    增加apihelper，增加依赖注入，增加api异常处理

---
+++ Exam/Exam.Api/App_Start/WebApiExceptionFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Exam.Api.App_Start
+{
+    /// <summary>
+    /// 异常处理类
+    /// </summary>
+    public class WebApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>
+        /// 重写基类的异常处理方法
+        /// </summary>
+        /// <param name="actionExecutedContext"></param>
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            HttpResponseMessage response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.OK);
+            string jsonStr = JsonConvert.SerializeObject(new { Success = false, Msg = actionExecutedContext.Exception.Message, Data = "" });
+            response.Content = new StringContent(jsonStr, Encoding.UTF8);
+            actionExecutedContext.Response = response;
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
--- Exam/Exam.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ Exam/Exam.Api/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -35,7 +35,7 @@ namespace Exam.Api.Areas.HelpPage
         {
             //// Uncomment the following to use the documentation from XML documentation file.
             //config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
-
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/XML/APIHelper.xml")));
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
             //// formats by the available formatters.
