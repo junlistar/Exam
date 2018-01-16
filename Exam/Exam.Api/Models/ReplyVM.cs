0f2358a2ac36de96da3da72fe4379e50bc3f4457
zhiwei.ni <510685792@qq.com>
2018-01-16 15:57:39 +0800

    问题模块

---
+++ Exam/Exam.Api/Models/ReplyVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Exam.Domain.Model;
+
+namespace Exam.Api.Models
+{
+    /// <summary>
+    /// 回复模型
+    /// </summary>
+    public class ReplyVM
+    {
+        public  int ReplyId { get; set; }
+        public  string Content { get; set; }
+        public  int UserInfoId { get; set; }
+        public  int QuestionId { get; set; }
+        public  int Reads { get; set; }
+        public  int Sort { get; set; }
+
+        public  DateTime CTime { get; set; }
+        public  UserInfo UserInfo { get; set; }
+    }
+}
