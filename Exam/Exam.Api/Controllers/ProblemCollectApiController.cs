d7f2ce40b27ac641aaedf01e4ba470ddd0e7117c
zhiwei.ni <510685792@qq.com>
2018-01-16 16:30:23 +0800

    1

---
+++ Exam/Exam.Api/Controllers/ProblemCollectApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Exam.Core.Infrastructure;
+using Exam.IService;
+
+namespace Exam.Api.Controllers
+{
+    /// <summary>
+    /// 收藏模块
+    /// </summary>
+    public class ProblemCollectApiController : ApiController
+    {
+        //private readonly IProblemCollectService _newsInfoService = EngineContext.Current.Resolve<IProblemCollectService>();
+    }
+}
