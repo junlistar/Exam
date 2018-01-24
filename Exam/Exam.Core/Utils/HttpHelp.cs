bb97e71115774e935d04fc7917c79da69b330810
EDAN <EDAN@DESKTOP-99AUGL9>
2018-01-24 13:16:48 +0800

    修改抓取方法

--- Exam/Exam.Core/Utils/HttpHelp.cs
+++ Exam/Exam.Core/Utils/HttpHelp.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exam.Core.Utils
@@ -118,6 +119,64 @@ namespace Exam.Core.Utils
                 webClient.UploadDataCompleted += callBackUploadDataCompleted;
 
             webClient.UploadDataAsync(new Uri(url), "POST", sendData);
+        }
+
+
+        /// <summary>  
+        /// 清除文本中Html的标签  
+        /// </summary>  
+        /// <param name="Content"></param>  
+        /// <returns></returns>  
+        public static string ClearHtml(string Content)
+        {
+            Content = Zxj_ReplaceHtml("&#[^>]*;", "", Content);
+            Content = Zxj_ReplaceHtml("</?marquee[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?object[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?param[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?embed[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?table[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml(" ", "", Content);
+            Content = Zxj_ReplaceHtml("</?tr[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?th[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?p[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?a[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?img[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?tbody[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?li[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?span[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?div[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?th[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?td[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?script[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("(javascript|jscript|vbscript|vbs):", "", Content);
+            Content = Zxj_ReplaceHtml("on(mouse|exit|error|click|key)", "", Content);
+            Content = Zxj_ReplaceHtml("<\\?xml[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("<\\/?[a-z]+:[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?font[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?b[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?u[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?i[^>]*>", "", Content);
+            Content = Zxj_ReplaceHtml("</?strong[^>]*>", "", Content);
+            string clearHtml = Content;
+            return clearHtml;
+        }
+
+        /// <summary>  
+        /// 清除文本中的Html标签  
+        /// </summary>  
+        /// <param name="patrn">要替换的标签正则表达式</param>  
+        /// <param name="strRep">替换为的内容</param>  
+        /// <param name="content">要替换的内容</param>  
+        /// <returns></returns>  
+        private static string Zxj_ReplaceHtml(string patrn, string strRep, string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                content = "";
+            }
+            Regex rgEx = new Regex(patrn, RegexOptions.IgnoreCase);
+            string strTxt = rgEx.Replace(content, strRep);
+            return strTxt;
         }
     }
 }
--- Exam/Exam.Service/GrabTopicService.cs
+++ Exam/Exam.Service/GrabTopicService.cs
@@ -1,7 +1,9 @@
-﻿using Exam.IService;
+﻿using Exam.Core.Utils;
+using Exam.IService;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -16,7 +18,7 @@ namespace Exam.Service
         public static CookieContainer cc = new CookieContainer();//维持cookie或Session
 
         public static string cookie = "";
-
+        public static string httpHead = "http://zk.0373kj.com";
         /// <summary>
         /// 抓取题库服务
         /// </summary>
@@ -29,7 +31,7 @@ namespace Exam.Service
                 case "注会":
                     //Task.Run(() =>
                     //{
-                        CPA();
+                    CPA();
                     //});
                     break;
                 default:
@@ -42,26 +44,37 @@ namespace Exam.Service
         /// </summary>
         public void CPA()
         {
-            
+
             //获取token
-            var url1 = "http://zk.0373kj.com/Account/UserLogin?Account=18707928905&WeiXinOpenId=";
+            var url1 = httpHead+"/Account/UserLogin?Account=18707928905&WeiXinOpenId=";
             //获取科目列表
-            var url2 = "http://zk.0373kj.com/Subjects/Chapter.php?tid=0&t="+DateTime.Now.ToString("yyyyMMddhhmmss");
+            var url2 = httpHead + "/Subjects/Chapter.php?tid=0&t=" + DateTime.Now.ToString("yyyyMMddhhmmss");
             //获取分类详细题目
-            var url3 = "http://zk.0373kj.com/Topic/GetQuestionList?sctid={0}&pagesize=10&page=1";
+            var url3 = httpHead + "/Topic/GetQuestionList?sctid={0}&pagesize=10&page=1";
             //获取登录cookie
-            var url4 = "http://zk.0373kj.com/Account/UserLogin";
-            if (GetToken(url1,url4))
+            var url4 = httpHead + "/Account/UserLogin";
+
+            var url5 = httpHead + "/Subjects/GetSectionList";
+            if (GetToken(url1, url4))
             {
                 //获取科目列表
-                GetSubjectsList(url2);
+                var subList = GetSubjectsList(url2).ToList();
+                //遍历科目列表，取得列表对应的
+                foreach (LinkAndTile item in subList)
+                {
+                    //DateTime.Now.Subtract(DateTime.Parse("1970-1-1")).TotalMilliseconds
+                    var str = GetHtml1(url5, "logoutnum=20; urltimestamp=2018120; LogonAccount=18707928905;");
+                    //pageindex=1&type=0&courseId =1&pagesize=10
+
+
+                }
             }
 
         }
         /// <summary>
         /// 获取token
         /// </summary>
-        public static bool GetToken(string url,string url4)
+        public static bool GetToken(string url, string url4)
         {
 
             //formData用于保存提交的信息
@@ -100,31 +113,117 @@ namespace Exam.Service
 
             return true;
         }
-        /// <summary>
-        /// 获取科目列表
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        public static bool GetSubjectsList(string url) {
+
+
+        public static string GetHtml(string url,string addCookie) {
+
+            //DateTime.Now.Subtract(DateTime.Parse("1970-1-1")).TotalMilliseconds
+            //var str = GetHtml(item.Link, "logoutnum=20; urltimestamp=2018120; LogonAccount=18707928905;");
+            //pageindex=1&type=0&courseId =1&pagesize=10
+            //http://zk.0373kj.com/Subjects/GetSectionList?_=1516768958691
+
+
+
+
             HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url);
             //string cookhead = HttpContext.Current.Application["cookieHeader"].ToString();
             string cookhead = cookie;
             request1.Method = "GET";
-            request1.Headers.Add("cookie:" + cookhead);
+            request1.Headers.Add("cookie:" + addCookie + cookhead);
             request1.KeepAlive = true;
             request1.AllowAutoRedirect = true;
 
             HttpWebResponse response1 = (HttpWebResponse)request1.GetResponse();
             Stream stream2 = response1.GetResponseStream();//获得回应的数据流
                                                            //将数据流转成 String
-            string result1 = new StreamReader(stream2, System.Text.Encoding.UTF8).ReadToEnd();
-            var match = Regex.Matches(result1, "<a.*?</a>");
-            var subList = new List<string>();
+            return new StreamReader(stream2, System.Text.Encoding.UTF8).ReadToEnd();
+        }
+
+        public static string GetHtml1(string url, string addCookie)
+        {
+
+            //DateTime.Now.Subtract(DateTime.Parse("1970-1-1")).TotalMilliseconds
+            //var str = GetHtml(item.Link, "logoutnum=20; urltimestamp=2018120; LogonAccount=18707928905;");
+            //pageindex=1&type=0&courseId =1&pagesize=10
+            //http://zk.0373kj.com/Subjects/GetSectionList?_=1516768958691
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            long t = (DateTime.Now.Ticks - startTime.Ticks) / 10000;
+            url += "?_=" + t;
+            //formData用于保存提交的信息
+            string formData = "pageindex=1&type=0&courseId =1&pagesize=10";
+
+
+//        Accept: */*
+//Accept-Encoding:gzip, deflate
+//Accept-Language:zh-CN,zh;q=0.9
+//Content-Length:63
+//Content-Type:application/x-www-form-urlencoded
+//Cookie:ASP.NET_SessionId=32fttu3g21cgthsuh5bsholi; urltimestamp=20181; usertoken=da57c562c956a502409f3403f3feec872422415039
+//Host:zk.0373kj.com
+//Origin:http://zk.0373kj.com
+//Proxy-Connection:keep-alive
+//Referer:http://zk.0373kj.com/Subjects/List.php?scid=1&tid=0&t=201801241311
+//User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36
+//X-Requested-With:XMLHttpRequest
+
+
+            //if (formData.Length > 0)
+            //    formData = formData.Substring(0, formData.Length - 1); //去除最后一个 '&'
+
+            //把提交的信息转码（post提交必须转码）
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(formData);
+
+            HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(url);
+            //string cookhead = HttpContext.Current.Application["cookieHeader"].ToString();
+            string cookhead = cookie;
+            request1.Method = "POST";
+            request1.Accept = "*/*";
+            request1.ContentLength = data.Length;
+            request1.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+            request1.Headers.Add("cookie:" + cookhead+ ";urltimestamp=20181");
+            request1.KeepAlive = true;
+            request1.AllowAutoRedirect = true;
+            Stream newStream = request1.GetRequestStream();
+            newStream.Write(data, 0, data.Length);//将请求的信息写入request
+            newStream.Close();
+            HttpWebResponse response1 = (HttpWebResponse)request1.GetResponse();
+            Stream stream2 = response1.GetResponseStream();//获得回应的数据流
+                                                           //将数据流转成 String
+            return new StreamReader(stream2, System.Text.Encoding.UTF8).ReadToEnd();
+        }
+
+        /// <summary>
+        /// 获取科目列表
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static IEnumerable<LinkAndTile> GetSubjectsList(string url)
+        {
+            string result1 = GetHtml(url,"");
+            var match = Regex.Matches(result1, @"<a href=.*?>([\s\S]*?)</a>");
+            var subList = new List<LinkAndTile>();
             foreach (Match m in match)
             {
-                subList.Add(m.Value);
+                var link = httpHead + Regex.Match(m.Value, "href=.*?>").Value.Replace("href=\"", "")
+                    .Replace("\">", "").Replace("/r", "").Replace("/n", "").Trim();
+                var title = HttpHelp.ClearHtml(m.Value);
+                subList.Add(new LinkAndTile()
+                {
+                    Link = link,
+                    Title = title
+                });
             }
-            return true;
+            return subList;
         }
+
+    }
+    /// <summary>
+    /// 科目列表页面
+    /// </summary>
+    public class LinkAndTile
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
     }
 }
