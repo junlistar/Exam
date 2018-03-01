431a163c7286797d4d04f5b5a53fa9c3435c91b4
jun.li <450152445@qq.com>
2018-03-01 09:39:35 +0800

    添加考试记录页面模型

---
+++ Exam/Exam.Admin/Models/UserExamClassVM.cs
@@ -0,0 +1,33 @@
+﻿using Exam.Admin.Models;
+using Exam.Core.Utils;
+using Exam.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam.Admin.Models
+{
+    public class UserExamClassVM
+    {
+        public int V { get; set; }
+
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; } 
+        /// <summary>
+        /// 分页
+        /// </summary>
+        public Paging<UserExamClass> Paging { get; set; }
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        public Paging<UserExamProblem> DetailPaging { get; set; }
+
+        //查询条件
+        public string QueryProblemTitle{ get; set; } 
+        public int QueryUserInfoId{ get; set; }
+    }
+}
